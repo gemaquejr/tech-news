fcab86fdddf96cb180b8326e3e7120a746752152
gemaquejr <gemaquejr@hotmail.com>
2023-01-22 13:17:34 -0300

    Adiciona funcao fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    import requests
+    import time
+
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
