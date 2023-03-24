3f37a781a0183836e48db406c00ef7dc7749ea3a
gemaquejr <gemaquejr@hotmail.com>
2023-03-24 10:48:16 -0300

    Adiciona a funcao top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
 # Requisito 10
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.tech_news
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    news_list = list(db.news.find().sort("comments_count", -1))
+    top_5 = news_list[:5]
+    return [(new["title"], new["url"]) for new in top_5]
 
 
 # Requisito 11
